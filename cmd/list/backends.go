db56036fab264e0cb326d30be75c7b9ea1986aff
Cameron Malone <cameronmkm2@gmail.com>
2024-03-12 09:35:14 -0500

    wip: cobra cli refactor -- update list docs
    Update docs for commands list/backends and list/wikis. Slight changes
    to the help strings in the command definition.

--- cmd/list/backends.go
+++ cmd/list/backends.go
@@ -7,14 +7,16 @@ import (
 	"github.com/spf13/cobra"
 )
 
+// Long message
 var backendsMsg = "List all backends currently supported by wikiscrape. A \"Backend\" here refers to the framework used to create the wiki site, as different providers will each have their own unique patterns for accessing and querying their APIs (if one exists at all) and so must be supported explicitly and on an individual basis."
 
+// Command
 var backendsCmd = &cobra.Command{
 	Use:   "backends",
-	Short: "List supported backends by name",
+	Short: "list supported backends",
 	Long:  backendsMsg,
 	Args:  cobra.NoArgs,
-	Run: func(cmd *cobra.Command, args []string) {
+	Run: func(_ *cobra.Command, _ []string) {
 		backends := util.GetSupportedBackends()
 		for _, b := range backends {
 			fmt.Println(b)
--- cmd/list/wikis.go
+++ cmd/list/wikis.go
@@ -7,12 +7,14 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var longMsg = "List by name all supported wikis in the current version of wikiscrape. The items outputted by this command are designed as valid input for the \"--wiki\" flag.\n\nTo say a wiki is \"supported\" means that we have a record of the website's api query path, backend framework, and page prefix stored in internal/util/wikisupport.go AND that the backend is explicitly supported in internal/wiki"
+// Long message
+var wikisMsg = "List by name all supported wikis in the current version of wikiscrape. The items outputted by this command are designed as valid input for the \"--wiki\" flag in the \"get\" command and its subcommands.\n\nTo say a wiki is \"supported\" means that we have a record of the website's api query path, backend framework, and page prefix stored in internal/util/wikisupport.go AND that the backend is explicitly supported in internal/wiki."
 
+// Command
 var wikisCmd = &cobra.Command{
 	Use:   "wikis",
-	Short: "List supported wikis by name",
-	Long:  longMsg,
+	Short: "list supported wikis",
+	Long:  wikisMsg,
 	Args:  cobra.NoArgs,
 	Run: func(_ *cobra.Command, _ []string) {
 		wikis := util.GetSupportedWikis()
