d9bb82bc570ab777fd8a962d7a7fd551f351dc57
Cameron Malone <cameronmkm2@gmail.com>
2024-03-12 15:00:31 -0500

    wip: cobra cli refactor -- add pages cmd
    Add the pages command as a subcommand of get. Allows for users to
    scrape a list of pages whose names are defined in a file. The persistent
    section flag allows for the user to also scrape only specific sections
    from all pages. At the moment only json input is supported.

---
+++ cmd/get/pages.go
@@ -0,0 +1,49 @@
+package get
+
+import (
+	"fmt"
+
+	"github.com/mal0ner/wikiscrape/internal/manifest"
+	"github.com/mal0ner/wikiscrape/internal/util"
+	"github.com/spf13/cobra"
+)
+
+// Long message
+var pagesMsg = "Get and export a list of pages whose names are defined in a 'manifest' file. The persistent 'section' flag available to the get command and all its subcommands allows for retrieving specific sections from all pages in the manifest."
+
+// Flag vars
+var manFile string
+
+// Command
+var pagesCmd = &cobra.Command{
+	Use:   "pages",
+	Short: "Get all pages listed in a file",
+	Long:  pagesMsg,
+	Args:  cobra.NoArgs,
+	RunE: func(_ *cobra.Command, _ []string) error {
+		pageNames, err := manifest.ReadFrom(manFile)
+		if err != nil {
+			return err
+		}
+		queryData, err := util.GetQueryDataFromName(pageNames[0], wikiName)
+		if err != nil {
+			fmt.Println(err.Error())
+			return err
+		}
+		w, err := getWikiFromQueryData(queryData)
+		if err != nil {
+			return err
+		}
+		err = w.ScrapeAndExport(pageNames)
+		return nil
+	},
+}
+
+func init() {
+	pagesCmd.Flags().StringVarP(&manFile, "from-manifest", "f", "", "path to the manifest file")
+	pagesCmd.Flags().StringVarP(&wikiName, "wiki", "w", "", "name of the wiki you wish to scrape")
+	pagesCmd.MarkPersistentFlagRequired("wiki")
+	pagesCmd.MarkFlagRequired("from-manifest")
+	pagesCmd.MarkFlagRequired("wiki")
+	pagesCmd.MarkFlagFilename("from-manifest", "json")
+}
