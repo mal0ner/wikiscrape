231794c0286a106f60ca5ac7e4b8a06e8d3885d7
Cameron Malone <cameronmkm2@gmail.com>
2024-03-12 02:37:49 -0500

    wip: cobra cli refactor -- remove unused function from root command

--- cmd/root.go
+++ cmd/root.go
@@ -1,7 +1,6 @@
 package cmd
 
 import (
-	"fmt"
 	"os"
 
 	"github.com/mal0ner/wikiscrape/cmd/get"
@@ -13,7 +12,6 @@ var rootCmd = &cobra.Command{
 	Use:   "wikiscrape",
 	Short: "Scrape and export wiki pages!",
 	Long:  "A tool for scraping wikis running on a number of different backends and then exporting the data to different formats",
-	RunE:  getPageFromURL,
 	Args:  cobra.MatchAll(cobra.ExactArgs(1)),
 }
 
@@ -26,12 +24,4 @@ func Execute() {
 	if err := rootCmd.Execute(); err != nil {
 		os.Exit(1)
 	}
-}
-
-func getPageFromURL(cmd *cobra.Command, args []string) error {
-	// parse url for hostname and path
-	// use internal scraper to get data
-	// output to format
-	fmt.Println("Executing get page from url")
-	return nil
 }
